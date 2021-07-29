97b0888f07561d433294ef3922a4434ac0c55c5d
danny h <daf281@aol.com>
2021-07-29 18:25:41 +0000

    finished day 13

--- starter/firebaseNotes/public/js/viewNotes.js
+++ starter/firebaseNotes/public/js/viewNotes.js
@@ -1,12 +1,14 @@
+let googleUser;
+let currentEditingNoteId;
+
 window.onload = (event) => {
-  // Use this to retain user state between html pages.
-  firebase.auth().onAuthStateChanged(function(user) {
+  firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       console.log('Logged in as: ' + user.displayName);
+      googleUser = user;
       const googleUserId = user.uid;
       getNotes(googleUserId);
     } else {
-      // If not logged in, navigate back to login page.
       window.location = 'index.html'; 
     };
   });
@@ -16,32 +18,69 @@ const getNotes = (userId) => {
   const notesRef = firebase.database().ref(`users/${userId}`);
   notesRef.on('value', (snapshot) => {
     const data = snapshot.val();
-    renderData(data);
+    renderCard(data);
   });
 };
 
-const renderData = (data) => {
-    const destination = document.querySelector('#app');
-    destination.innerHTML = "";
-    for (let key in data) {
-        const note = data[key];
-        destination.innerHTML += createCard(note);
-    }
+const renderCard = (data) => {
+  let cards = ``;
+  for(const key in data) {
+    const note = data[key];
+    cards += createCard(note, key);
+
+  }
+  document.querySelector('#app').innerHTML = cards;
 };
 
-const createCard = (note) => {
-    return `<div class="column is-one-quarter">
-                <div class="card"> 
-                    <header class="card-header"> 
-                        <p class="card-header-title"> 
-                            ${note.title} 
-                        </p> 
-                    </header> 
-                    <div class="card-content"> 
-                        <div class="content">
-                            ${note.text} 
-                        </div>
-                    </div> 
-                </div>
-            </div>`;
+const createCard = (note, noteId) => {
+  return `<div class="column is-one-quarter">
+    <div class="card">
+        <header class="card-header">
+            <p class="card-header-title">
+            ${note.title}
+            </p>
+        </header>
+        <div class="card-content">
+            <div class="content">
+            ${note.text}
+            </div>
+        </div>
+        <footer class="card-footer">
+            <a href="#" class="card-footer-item" onclick="editNote('${noteId}')">Edit</a>
+            <a href="#" class="card-footer-item" onclick="deleteNote('${noteId}')">Delete</a>
+        </footer>
+    </div>
+  </div>`;
 };
+
+const deleteNote = (noteId) => {
+    console.log('delete');
+    if(confirm('Are you sure you want to delete this note?'))
+        firebase.database().ref(`users/${googleUser.uid}/${noteId}`).remove().then(e => console.log('done')).catch(e => console.log(e));
+}
+
+const editNote = (noteId) => {
+
+    // it would be better to not make this db call at all because 
+    // we already have this data in the html or could send the data
+    // to this function via parameters
+    firebase.database().ref(`users/${googleUser.uid}/`).once('value', (snapshot) => {
+        const data = snapshot.val();
+        const noteDetails = data[noteId];
+        document.querySelector('#noteTitleInput').value = noteDetails.title;
+        document.querySelector('#noteTextInput').value = noteDetails.text;
+        document.querySelector('#editNoteModal').classList.add('is-active');
+        currentEditingNoteId = noteId;
+    });
+}
+
+const closeModal = () =>  {
+    document.querySelector('#editNoteModal').classList.remove('is-active');
+}
+
+const saveChanges = () => {
+    firebase.database().ref(`users/${googleUser.uid}/${currentEditingNoteId}`).update({
+        title : document.querySelector('#noteTitleInput').value,
+        text : document.querySelector('#noteTextInput').value
+    }).then(e => closeModal()).catch(e => console.log(e));
+}
